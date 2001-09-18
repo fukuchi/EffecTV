76871e50519460b0879894d273faf7077d054b73
Kentaro Fukuchi <fukuchi@users.sourceforge.net>
2001-09-18 08:16:28 +0000

    Bugfixes.

--- effects/ripple.c
+++ effects/ripple.c
@@ -22,7 +22,7 @@ int rippleEvent(SDL_Event *event);
 static int mode = 0; // 0 = motion detection / 1 = rain
 static char *effectname = "RippleTV";
 static int stat;
-static int *buffer;
+static signed char *vtable;
 static int *map;
 static int *map1, *map2;
 static int map_h, map_w;
@@ -48,13 +48,15 @@ effect *rippleRegister()
 	effect *entry;
 	
 	sharedbuffer_reset();
-	map_h = video_height / 2;
-	map_w = video_width / 2;
+	map_h = video_height / 2 + 1;
+	map_w = video_width / 2 + 1;
 	map = (int *)sharedbuffer_alloc(map_h*map_w*2*sizeof(int));
-	buffer = (int *)sharedbuffer_alloc(video_area*sizeof(int));
-	if(map == NULL || buffer == NULL) {
+	vtable = (signed char *)sharedbuffer_alloc(map_h*map_w*2*sizeof(signed char));
+	if(map == NULL || vtable == NULL) {
 		return NULL;
 	}
+
+	bzero(vtable, map_h*map_w*2*sizeof(signed char));
 
 	entry = (effect *)malloc(sizeof(effect));
 	if(entry == NULL) {
@@ -119,13 +121,59 @@ static void motiondetect(RGB32 *src)
 			diff += 2;
 		}
 		diff += width+2;
-		p++;
-		q++;
+		p+=2;
+		q+=2;
 	}
 }
 
 static void raindrop()
 {
+	static int period = 0;
+	static int rain_stat = 0;
+	static unsigned int drop_prob = 0;
+	static int drop_prob_increment = 0;
+	static int drop_power = 0;
+
+	int x, y;
+	int *p;
+
+	if(period == 0) {
+		switch(rain_stat) {
+		case 0:
+			period = (fastrand()>>20)+100;
+			drop_prob = 0;
+			drop_prob_increment = ((fastrand()>>18)+0x7fff)/period;
+			drop_power = (-(fastrand()>>28)-2)<<point;
+			rain_stat = 1;
+			break;
+		case 1:
+			period = (fastrand()>>22)+1000;
+			drop_prob_increment = 0;
+			rain_stat = 2;
+			break;
+		case 2:
+			period = (fastrand()>>24)+60;
+			drop_prob_increment = -(drop_prob/period);
+			rain_stat = 3;
+			break;
+		case 3:
+		default:
+			period = (fastrand()>>23)+500;
+			drop_prob = 0;
+			rain_stat = 0;
+		}
+	}
+	if(rain_stat>0) {
+		if((fastrand()>>16)<drop_prob) {
+			x = fastrand()%(map_w-4)+2;
+			y = fastrand()%(map_h-4)+2;
+			p = map1 + y*map_w + x;
+			*p = drop_power;
+			*(p-map_w) = *(p-1) = *(p+1) = *(p+map_w) = drop_power/2;
+		}
+		drop_prob += drop_prob_increment;
+	}
+	period--;
 }
 
 int rippleDraw()
@@ -136,6 +184,7 @@ int rippleDraw()
 	int width, height;
 	int *p, *q;
 	RGB32 *src, *dest;
+	signed char *vp;
 
 	if(video_syncframe())
 		return -1;
@@ -173,19 +222,17 @@ int rippleDraw()
 	}
 
 	i = 0;
+	vp = vtable;
 	p = map1;
 	for(y=height-1; y>0; y--) {
 		for(x=width-1; x>0; x--) {
-			h = p[0];
-			buffer[i] = h;
-			buffer[i+1] = (h+p[1])/2;
-			buffer[i+video_width] = (h+p[width])/2;
-			buffer[i+video_width+1] = (h+p[1]+p[width]+p[width+1])/4;
+			vp[0] = sqrtable[((p[0] - p[1] + (1<<(point-3)))>>(point-2))&0xff]; 
+			vp[1] = sqrtable[((p[0] - p[width] + (1<<(point-3)))>>(point-2))&0xff]; 
 			p++;
-			i += 2;
+			vp+=2;
 		}
 		p++;
-		i += video_width+2;
+		vp+=2;
 	}
 	if(screen_mustlock()) {
 		if(screen_lock() < 0) {
@@ -198,38 +245,68 @@ int rippleDraw()
 		dest = (RGB32 *)screen_getaddress();
 	}
 
-	i = 0;
 	height = video_height;
 	width = video_width;
-#ifdef DEBUG
-	for(y=0; y<height-1; y++) {
-		for(x=0; x<width-1; x++) {
-			dx = x + sqrtable[((buffer[i] - buffer[i+1] + (1<<(point-3)))>>(point-2))&0xff];
-			dy = y + sqrtable[((buffer[i] - buffer[i+width] + (1<<(point-3)))>>(point-2))&0xff];
+	vp = vtable;
+#ifndef DEBUG
+	for(y=0; y<height; y+=2) {
+		for(x=0; x<width; x+=2) {
+			h = (int)vp[0];
+			v = (int)vp[1];
+			dx = x + h;
+			dy = y + v;
 			if(dx<0) dx=0;
 			if(dy<0) dy=0;
 			if(dx>width) dx=width;
 			if(dy>height) dy=height;
-			*dest++ = src[dy*width+dx];
-			i++;
+			dest[0] = src[dy*width+dx];
+
+			i = dx;
+
+			dx = x + (h+(int)vp[2])/2;
+			if(dx<0) dx=0;
+			if(dx>width) dx=width;
+			dest[1] = src[dy*width+dx];
+
+			dy = y + (v+(int)vp[map_w*2+1])/2;
+			if(dy<0) dy=0;
+			dest[width] = src[dy*width+i];
+
+			dest[width+1] = src[dy*width+dx];
+			dest+=2;
+			vp+=2;
 		}
-		i++;
-		dest++;
+		dest += video_width;
+		vp += 2;
 	}
 #else
-	for(y=0; y<height-1; y++) {
-		for(x=0; x<width-1; x++) {
-			dx = x + sqrtable[((buffer[i] - buffer[i+1] + (1<<(point-3)))>>(point-2))&0xff];
-			dy = y + sqrtable[((buffer[i] - buffer[i+width] + (1<<(point-3)))>>(point-2))&0xff];
-			if(dx<0) dx=0;
-			if(dy<0) dy=0;
-			if(dx>width) dx=width;
-			if(dy>height) dy=height;
-			*dest++ = src[dy*width+dx];
-			i++;
+	for(y=0; y<height; y+=2) {
+		for(x=0; x<width; x+=2) {
+			h = (int)vp[0] + (int)vp[1] + 128;
+			if(h<0) h=0;
+			if(h>255) h=255;
+			dest[0] = h<<8;
+
+			h = (int)vp[2] + (int)vp[1] + 128;
+			if(h<0) h=0;
+			if(h>255) h=255;
+			dest[1] = h<<8;
+
+			h = (int)vp[0] + (int)vp[map_w*2+1] + 128;
+			if(h<0) h=0;
+			if(h>255) h=255;
+			dest[width] = h<<8;
+
+			h = (int)vp[2] + (int)vp[map_w*2+1] + 128;
+			if(h<0) h=0;
+			if(h>255) h=255;
+			dest[width+1] = h<<8;
+
+			dest+=2;
+			vp+=2;
 		}
-		i++;
-		dest++;
+		dest += video_width;
+		vp += 2;
 	}
 #endif
 	if(stretch) {
