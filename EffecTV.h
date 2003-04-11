d8fc3797079d6e4269ab2cb6f0bd68213ebbaa8f
Kentaro Fukuchi <fukuchi@users.sourceforge.net>
2003-04-11 06:35:55 +0000

    Some definitions are split to effect.h, so that each effect code now need not to include EffecTV.h.

--- EffecTV.h
+++ EffecTV.h
@@ -4,6 +4,20 @@
  *
  * EffecTV.h: common header
  *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  */
 
 #ifndef __EFFECTV_H__
@@ -36,19 +50,7 @@
 #define SDL_ENABLE 1
 #endif
 
-typedef unsigned int RGB32;
-#define PIXEL_SIZE (sizeof(RGB32))
-
-typedef struct _effect
-{
-	char *name;
-	int (*start)(void);
-	int (*stop)(void);
-	int (*draw)(void);
-	int (*event)(SDL_Event *);
-} effect;
-
-typedef effect *effectRegistFunc(void);
+#include "effect.h"
 
 extern int debug;
 
